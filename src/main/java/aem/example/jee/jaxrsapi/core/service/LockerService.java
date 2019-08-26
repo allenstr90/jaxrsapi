15a368902f4c50b08d09f85c74f4179e68b0ef69
Allens Torres Ruíz <allenstr90@gmail.com>
2019-08-26 09:53:39 -0400

    [add] user is locked and deactivate user

--- src/main/java/aem/example/jee/jaxrsapi/core/service/LockerService.java
+++ src/main/java/aem/example/jee/jaxrsapi/core/service/LockerService.java
@@ -2,15 +2,25 @@ package aem.example.jee.jaxrsapi.core.service;
 
 import aem.example.jee.jaxrsapi.core.exception.UserLockException;
 import aem.example.jee.jaxrsapi.core.model.LockUser;
+import aem.example.jee.jaxrsapi.core.model.User;
 import aem.example.jee.jaxrsapi.core.repository.LockUserRepository;
+import aem.example.jee.jaxrsapi.core.repository.TokenRefreshRepository;
+import aem.example.jee.jaxrsapi.core.repository.UserRepository;
 
 import javax.inject.Inject;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 public class LockerService {
     @Inject
     private LockUserRepository lockUserRepository;
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Inject
+    private TokenRefreshRepository tokenRefreshRepository;
 
     public void unlock(String username) {
         lockUserRepository.unlock(username);
@@ -18,11 +28,24 @@ public class LockerService {
 
     public void markToLock(String username) throws UserLockException {
         LockUser lockUser = lockUserRepository.checkUser(username);
-        if (lockUser.getTrays() + 1 > 3) {
+        if (lockUser.getTrays() == 3) {
             lockUser.setLocked(true);
             lockUser.setTo(Instant.now().plus(20, ChronoUnit.SECONDS));
             lockUserRepository.update(lockUser);
+            tokenRefreshRepository.removeUserRefreshToken(username);
             throw new UserLockException(username);
         }
+        if (lockUser.getTrays() + 1 > 5) {
+            Optional<User> byUsernameAndActive = userRepository.findByUsernameAndActive(username);
+            byUsernameAndActive.ifPresent(user -> {
+                user.setActive(false);
+                userRepository.updateUser(user);
+            });
+        }
+    }
+
+    public boolean userIsLocked(String username) {
+        Optional<LockUser> lockUser = lockUserRepository.getLockUser(username);
+        return lockUser.map(user -> (user.getTrays() + 1 > 3)).orElse(false);
     }
 }
